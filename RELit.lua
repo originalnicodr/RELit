ff525e20a3f24bb673d674e27b6fdc6ee26cd725
originalnicodr <niconavall@gmail.com>
2022-06-28 23:19:51 -0300

    Added working position sliders, worked on UI, and sanitized the code.

--- RELit.lua
+++ RELit.lua
@@ -5,16 +5,16 @@ local lightCounter = 0
 local gameName = reframework:get_game_name()
 
 function create_gameobj(name, component_names)
-    local new_gameobj = sdk.find_type_definition("via.GameObject"):get_method("create(System.String)"):call(nil, name)
-    if new_gameobj and new_gameobj:add_ref() and new_gameobj:call(".ctor") then
-        for i, comp_name in ipairs(component_names or {}) do 
-            local td = sdk.find_type_definition(comp_name)
-            local new_component = td and new_gameobj:call("createComponent(System.Type)", td:get_runtime_type())
-            if new_component and new_component:add_ref() then 
-                new_component:call(".ctor()")
+    local newGameobj = sdk.find_type_definition("via.GameObject"):get_method("create(System.String)"):call(nil, name)
+    if newGameobj and newGameobj:add_ref() and newGameobj:call(".ctor") then
+        for i, compName in ipairs(component_names or {}) do 
+            local td = sdk.find_type_definition(compName)
+            local newComponent = td and newGameobj:call("createComponent(System.Type)", td:get_runtime_type())
+            if newComponent and newComponent:add_ref() then 
+                newComponent:call(".ctor()")
             end
         end
-        return new_gameobj
+        return newGameobj
     end
 end
 
@@ -45,21 +45,21 @@ function dump(o)
     end
 end
  
-local function write_vec34(managed_object, offset, vector, doVec3)
-	if sdk.is_managed_object(managed_object) then 
-		managed_object:write_float(offset, vector.x)
-		managed_object:write_float(offset + 4, vector.y)
-		managed_object:write_float(offset + 8, vector.z)
-		if not doVec3 and vector.w then managed_object:write_float(offset + 12, vector.w) end
+local function write_vec34(managedObject, offset, vector, doVec3)
+	if sdk.is_managed_object(managedObject) then 
+		managedObject:write_float(offset, vector.x)
+		managedObject:write_float(offset + 4, vector.y)
+		managedObject:write_float(offset + 8, vector.z)
+		if not doVec3 and vector.w then managedObject:write_float(offset + 12, vector.w) end
 	end
 end
 
-local function write_mat4(managed_object, offset, mat4)
-	if sdk.is_managed_object(managed_object) then 
-		write_vec34(managed_object, offset, 	 mat4[0])
-		write_vec34(managed_object, offset + 16, mat4[1])
-		write_vec34(managed_object, offset + 32, mat4[2])
-		write_vec34(managed_object, offset + 48, mat4[3])
+local function write_mat4(managedObject, offset, mat4)
+	if sdk.is_managed_object(managedObject) then 
+		write_vec34(managedObject, offset, 	 mat4[0])
+		write_vec34(managedObject, offset + 16, mat4[1])
+		write_vec34(managedObject, offset + 32, mat4[2])
+		write_vec34(managedObject, offset + 48, mat4[3])
 	end
 end
 
@@ -79,27 +79,26 @@ local function ternary(cond, T, F)
 end
 
 local function add_new_light(lTable, createSpotLight, lightNo)
-
 	local componentToCreate = ternary(createSpotLight, "via.render.SpotLight", "via.render.PointLight")
-    local new_light = create_gameobj(ternary(createSpotLight, "Spotlight ", "Pointlight ")..tostring(lightNo), {componentToCreate})
-	local light_props = lua_find_component(new_light, componentToCreate)
+    local newLight = create_gameobj(ternary(createSpotLight, "Spotlight ", "Pointlight ")..tostring(lightNo), {componentToCreate})
+	local lightProps = lua_find_component(newLight, componentToCreate)
 	
-    light_props:call("set_Enabled", true)
-    light_props:call("set_Color", Vector3f.new(1, 1, 1))
-    light_props:call("set_Intensity", 1000.0)
-	light_props:call("set_ImportantLevel", 0)
-	light_props:call("set_BlackBodyRadiation", false)
-	light_props:call("set_UsingSameIntensity", false)
-	light_props:call("set_BackGroundShadowEnable", false)
-    light_props:call("set_ShadowEnable", true)
+    lightProps:call("set_Enabled", true)
+    lightProps:call("set_Color", Vector3f.new(1, 1, 1))
+    lightProps:call("set_Intensity", 1000.0)
+	lightProps:call("set_ImportantLevel", 0)
+	lightProps:call("set_BlackBodyRadiation", false)
+	lightProps:call("set_UsingSameIntensity", false)
+	lightProps:call("set_BackGroundShadowEnable", false)
+    lightProps:call("set_ShadowEnable", true)
 
-    move_light_to_camera(new_light)
-	light_props:call("update")
+    move_light_to_camera(newLight)
+	lightProps:call("update")
 	
     lightTableEntry = {
 		id = lightNo,
-        light = new_light,
-        light_props = light_props,
+        light = newLight,
+        lightProps = lightProps,
         showLightEditor = false,
         attachedToCam = false,
 		typeDescription = ternary(createSpotLight, "Spotlight ", "Pointlight "),
@@ -109,87 +108,123 @@ local function add_new_light(lTable, createSpotLight, lightNo)
     table.insert( lTable, lightTableEntry )
 end
 
-local function getNewLightNo()
+local function get_new_light_no()
 	lightCounter = lightCounter+1
 	return lightCounter
 end
 
 --UI---------------------------------------------------------
-local function handleFloatValue(light_props, captionString, getterFuncName, setterFuncName, stepSize, min, max)
-	changed, newValue = imgui.drag_float(captionString, light_props:call(getterFuncName), stepSize, min, max)
-	if changed then light_props:call(setterFuncName, newValue) end
-end
-
-local function handleBoolValue(light_props, captionString, getterFuncName, setterFuncName)
-	changed, enabledValue = imgui.checkbox(captionString, light_props:call(getterFuncName))
-	if changed then light_props:call(setterFuncName, enabledValue) end
-end
-
-re.on_draw_ui(function()
-    imgui.collapsing_header("RELit")
-
-    if imgui.button("Add new spotlight") then 
-        add_new_light(lightsTable, true, getNewLightNo())
-    end
+local function ui_margin()
+	imgui.text(" ")
 	imgui.same_line()
-    if imgui.button("Add new pointlight") then 
-        add_new_light(lightsTable, false, getNewLightNo())
+end
+
+local function handle_float_value(lightProps, captionString, getterFuncName, setterFuncName, stepSize, min, max)
+	ui_margin()
+	changed, newValue = imgui.drag_float(captionString, lightProps:call(getterFuncName), stepSize, min, max)
+	if changed then lightProps:call(setterFuncName, newValue) end
+end
+
+local function handle_bool_value(lightProps, captionString, getterFuncName, setterFuncName)
+	ui_margin()
+	changed, enabledValue = imgui.checkbox(captionString, lightProps:call(getterFuncName))
+	if changed then lightProps:call(setterFuncName, enabledValue) end
+end
+
+local function sliders_change_pos(object)
+    local objectTransform = object:call("get_Transform")
+    local objectPos = objectTransform:call("get_Position")
+
+	imgui.spacing()
+	ui_margin()
+
+	imgui.text("Position")
+	-- For some reason the z coordinate of the object isn't the height?
+	ui_margin()
+    changedX, newXValue = imgui.drag_float("X", objectPos.x, 0.01, -10000, 10000)
+	ui_margin()
+    changedZ, newZValue = imgui.drag_float("Y", objectPos.z, 0.01, -10000, 10000)
+	ui_margin()
+	changedY, newYValue = imgui.drag_float("Z", objectPos.y, 0.01, -10000, 10000)
+
+	imgui.spacing()
+
+    if changedX or changedY or changedZ then
+        if not changedX then newXValue = objectPos.x end
+        if not changedY then newYValue = objectPos.y end
+        if not changedZ then newZValue = objectPos.z end
+        objectTransform:set_position(Vector3f.new(newXValue, newYValue, newZValue))
     end
+end
 
-    imgui.spacing()
+function main_menu()
+	if imgui.tree_node("RELit") then
 
-    for i, lightEntry in ipairs(lightsTable) do
-        local light = lightEntry.light
-        local light_props = lightEntry.light_props
-
-        imgui.push_id(lightEntry.id)
-		local changed, enabledValue = imgui.checkbox("", light_props:call("get_Enabled"))
-		if changed then
-			light_props:call("set_Enabled", enabledValue)
+		if imgui.button("Add new spotlight") then 
+			add_new_light(lightsTable, true, get_new_light_no())
+		end
+		imgui.same_line()
+		if imgui.button("Add new pointlight") then 
+			add_new_light(lightsTable, false, get_new_light_no())
 		end
 
-		imgui.same_line()
+		imgui.spacing()
 
-		imgui.text(lightEntry.typeDescription..tostring(i))
-		imgui.same_line()
+		for i, lightEntry in ipairs(lightsTable) do
+			local light = lightEntry.light
+			local lightProps = lightEntry.lightProps
 
-		if imgui.button("Move To Camera") then 
-			move_light_to_camera(light)
+			imgui.push_id(lightEntry.id)
+			local changed, enabledValue = imgui.checkbox("", lightProps:call("get_Enabled"))
+			if changed then
+				lightProps:call("set_Enabled", enabledValue)
+			end
+
+			imgui.same_line()
+
+			imgui.text(lightEntry.typeDescription..tostring(i))
+			imgui.same_line()
+
+			if imgui.button("Move To Camera") then 
+				move_light_to_camera(light)
+			end
+
+			imgui.same_line()
+
+			local changed, attachedToCamValue = imgui.checkbox("Attach to camera", lightEntry.attachedToCam)
+			if changed then
+				lightEntry.attachedToCam = attachedToCamValue
+			end
+
+			imgui.same_line()
+
+			if imgui.button(" Edit ") then
+				lightEntry.showLightEditor = true
+			end
+
+			imgui.same_line()
+
+			if imgui.button("Delete") then 
+				light:call("destroy", light)
+				table.remove(lightsTable, i)
+			end
+
+			imgui.pop_id()
 		end
 
-		imgui.same_line()
+		imgui.text(" ")
+		imgui.text(" ")
+		imgui.tree_pop()
+	end
+end
 
-		local changed, attachedToCamValue = imgui.checkbox("Attach to camera", lightEntry.attachedToCam)
-		if changed then
-			lightEntry.attachedToCam = attachedToCamValue
-		end
-
-		imgui.same_line()
-
-		if imgui.button(" Edit ") then
-			lightEntry.showLightEditor = true
-		end
-
-		imgui.same_line()
-
-		if imgui.button("Delete") then 
-			light:call("destroy", light)
-			table.remove(lightsTable, i)
-		end
-
-		imgui.pop_id()
-    end
-
-    imgui.text(" ")
-    imgui.text(" ")
-end)
-
+re.on_draw_ui(main_menu)
 
 --Light Editor window UI-------------------------------------------------------
-re.on_frame(function()
-    for i, lightEntry in ipairs(lightsTable) do
+function light_editor_menu()
+	for i, lightEntry in ipairs(lightsTable) do
 		local light = lightEntry.light
-		local light_props = lightEntry.light_props
+		local lightProps = lightEntry.lightProps
 
 		if lightEntry.attachedToCam then
 			move_light_to_camera(light)
@@ -200,45 +235,55 @@ re.on_frame(function()
 			imgui.push_id(lightEntry.id)
             lightEntry.showLightEditor = imgui.begin_window(lightEntry.typeDescription..tostring(i).." editor", true, 64)
 
-			handleFloatValue(light_props, "Intensity", "get_Intensity", "set_Intensity", 1, 0, 100000)
+            sliders_change_pos(light)
+
+			handle_float_value(lightProps, "Intensity", "get_Intensity", "set_Intensity", 1, 0, 100000)
+
+			imgui.spacing()
+			ui_margin()
 			
-			changed, new_color = imgui.color_picker3("Light color", light_props:call("get_Color"))
+			changed, new_color = imgui.color_picker3("Light color", lightProps:call("get_Color"))
 			if changed then
-				light_props:call("set_Color", new_color)
+				lightProps:call("set_Color", new_color)
 			end
+
+			imgui.spacing()
 
 			if gameName~="dmc5" then
 				-- temperature settings don't work for some reason in DMC5
-				handleBoolValue(light_props, "Use temperature", "get_BlackBodyRadiation", "set_BlackBodyRadiation")
-				handleFloatValue(light_props, "Temperature", "get_Temperature", "set_Temperature", 10, 0, 10000)
+				handle_bool_value(lightProps, "Use temperature", "get_BlackBodyRadiation", "set_BlackBodyRadiation")
+				handle_float_value(lightProps, "Temperature", "get_Temperature", "set_Temperature", 10, 0, 10000)
 			end
-			handleFloatValue(light_props, "Bounce intensity", "get_BounceIntensity", "set_BounceIntensity", 0.01, 0, 1000)
-			handleFloatValue(light_props, "Min roughness", "get_MinRoughness", "set_MinRoughness", 0.01, 0, 1.0)
-			handleFloatValue(light_props, "AO Efficiency", "get_AOEfficiency", "set_AOEfficiency", 0.0001, 0, 10)
-			handleFloatValue(light_props, "Volumetric scattering intensity", "get_VolumetricScatteringIntensity", "set_VolumetricScatteringIntensity", 0.01, 0, 100000)
+			handle_float_value(lightProps, "Bounce intensity", "get_BounceIntensity", "set_BounceIntensity", 0.01, 0, 1000)
+			handle_float_value(lightProps, "Min roughness", "get_MinRoughness", "set_MinRoughness", 0.01, 0, 1.0)
+			handle_float_value(lightProps, "AO Efficiency", "get_AOEfficiency", "set_AOEfficiency", 0.0001, 0, 10)
+			handle_float_value(lightProps, "Volumetric scattering intensity", "get_VolumetricScatteringIntensity", "set_VolumetricScatteringIntensity", 0.01, 0, 100000)
 
 			-- Have no idea what this is
-			--changed, newValue = imgui.drag_float("Unit", light_props:call("get_Unit"), 0.01, 0, 100000)
-			--if changed then light_props:call("set_Unit", newValue) end
+			--changed, newValue = imgui.drag_float("Unit", lightProps:call("get_Unit"), 0.01, 0, 100000)
+			--if changed then lightProps:call("set_Unit", newValue) end
 
-			handleFloatValue(light_props, "Radius", "get_Radius", "set_Radius", 0.01, 0, 100000)
-			handleFloatValue(light_props, "Illuminance Threshold", "get_IlluminanceThreshold", "set_IlluminanceThreshold", 0.01, 0, 100000)
+			handle_float_value(lightProps, "Radius", "get_Radius", "set_Radius", 0.01, 0, 100000)
+			handle_float_value(lightProps, "Illuminance Threshold", "get_IlluminanceThreshold", "set_IlluminanceThreshold", 0.01, 0, 100000)
 
 			if lightEntry.isSpotLight then
-				handleFloatValue(light_props, "Cone", "get_Cone", "set_Cone", 0.01, 0, 1000)
-				handleFloatValue(light_props, "Spread", "get_Spread", "set_Spread", 0.01, 0, 100)
-				handleFloatValue(light_props, "Falloff", "get_Falloff", "set_Falloff", 0.01, 0, 100)
+				handle_float_value(lightProps, "Cone", "get_Cone", "set_Cone", 0.01, 0, 1000)
+				handle_float_value(lightProps, "Spread", "get_Spread", "set_Spread", 0.01, 0, 100)
+				handle_float_value(lightProps, "Falloff", "get_Falloff", "set_Falloff", 0.01, 0, 100)
 			end
-			handleBoolValue(light_props, "Enable shadows", "get_ShadowEnable", "set_ShadowEnable")
-			handleFloatValue(light_props, "Shadow bias", "get_ShadowBias", "set_ShadowBias", 0.0000001, 0, 1.0)
-			handleFloatValue(light_props, "Shadow blur", "get_ShadowVariance", "set_ShadowVariance", 0.0001, 0, 1.0)
-			handleFloatValue(light_props, "Shadow lod bias", "get_ShadowLodBias", "set_ShadowLodBias", 0.0000001, 0, 1.0)
-			handleFloatValue(light_props, "Shadow depth bias", "get_ShadowDepthBias", "set_ShadowDepthBias", 0.0000001, 0, 1.0)
-			handleFloatValue(light_props, "Shadow slope bias", "get_ShadowSlopeBias", "set_ShadowSlopeBias", 0.0000001, 0, 1.0)
-			handleFloatValue(light_props, "Shadow near plane", "get_ShadowNearPlane", "set_ShadowNearPlane", 0.00001, 0, 1.0)
+			
+			imgui.spacing()
+
+			handle_bool_value(lightProps, "Enable shadows", "get_ShadowEnable", "set_ShadowEnable")
+			handle_float_value(lightProps, "Shadow bias", "get_ShadowBias", "set_ShadowBias", 0.0000001, 0, 1.0)
+			handle_float_value(lightProps, "Shadow blur", "get_ShadowVariance", "set_ShadowVariance", 0.0001, 0, 1.0)
+			handle_float_value(lightProps, "Shadow lod bias", "get_ShadowLodBias", "set_ShadowLodBias", 0.0000001, 0, 1.0)
+			handle_float_value(lightProps, "Shadow depth bias", "get_ShadowDepthBias", "set_ShadowDepthBias", 0.0000001, 0, 1.0)
+			handle_float_value(lightProps, "Shadow slope bias", "get_ShadowSlopeBias", "set_ShadowSlopeBias", 0.0000001, 0, 1.0)
+			handle_float_value(lightProps, "Shadow near plane", "get_ShadowNearPlane", "set_ShadowNearPlane", 0.00001, 0, 1.0)
 
 			if lightEntry.isSpotLight then
-				handleFloatValue(light_props, "Detail shadow", "get_DetailShadow", "set_DetailShadow", 0.001, 0, 1.0)
+				handle_float_value(lightProps, "Detail shadow", "get_DetailShadow", "set_DetailShadow", 0.001, 0, 1.0)
 			end 
 			
 			imgui.spacing()
@@ -251,8 +296,9 @@ re.on_frame(function()
 
             imgui.end_window()
 			imgui.pop_id()
-			light_props:call("update")
+			lightProps:call("update")
         end
     end
-end)
--------------------------------------------------------------------------
+end
+
+re.on_frame(light_editor_menu)
